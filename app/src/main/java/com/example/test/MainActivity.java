7537a0ab562e74de8822d7261e115e40288f1422
jho5245 <jho5245@naver.com>
2024-03-19 14:18:17 +0900

    Add an example intent and button

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		EdgeToEdge.enable(this);
+		setContentView(R.layout.activity_main); // 앱을 실행했을 때 어떤게 보일지(초기화면)
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+		{
+			Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+			return insets;
+		});
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main); // 앱을 실행했을 때 어떤게 보일지(초기화면)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+	public void onClicked(View view)
+	{
+		Log.i(System.getProperty("os.name"), "test_message");
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-2911-5245"));
+		startActivity(intent);
+	}
 }
